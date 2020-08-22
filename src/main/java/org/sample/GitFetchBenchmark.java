c31b4fa4a484024c8df310822aee1cc1dd50cecf
Rishabh Budhouliya <31189405+rishabhBudhouliya@users.noreply.github.com>
2020-08-23 00:29:19 +0530

    Update GitFetchBenchmark.java

--- src/main/java/org/sample/GitFetchBenchmark.java
+++ src/main/java/org/sample/GitFetchBenchmark.java
@@ -89,12 +89,11 @@ public class GitFetchBenchmark {
          * Cairo: (93.54 MiB) https://github.com/cairoshell/cairoshell.git
          * Samba: (324.26 MiB) https://github.com/samba-team/samba.git
          */
-        @Param({"https://github.com/omkar-dsd/sample-base-repository.git",
-                "https://github.com/omkar-dsd/sample-10-commit.git",
-                "https://github.com/omkar-dsd/sample-100-commit.git",
-                "https://github.com/omkar-dsd/sample-1000-commit.git",
-                "https://github.com/omkar-dsd/sample-5000-commit.git",
-		"https://github.com/omkar-dsd/sample-50000-commit.git"})
+        @Param({"https://github.com/stephenc/java-logging-benchmarks.git",
+                "https://github.com/uutils/coreutils.git",
+                "https://github.com/jenkinsci/git-plugin.git",
+                "https://github.com/jenkinsci/analysis-model.git",
+                "https://github.com/jenkinsci/jenkins.git"})
         String repoUrl;
 
         private File cloneUpstreamRepositoryLocally(File parentDir, String repoUrl) throws Exception {
